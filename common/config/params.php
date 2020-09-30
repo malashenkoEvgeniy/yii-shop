d1552e96e8bd45a16b9bbebb4e529a2eaf8abe43
Evgeniy Malashenko <111k1808@gmail.com>
2020-09-30 23:38:06 +0300

    init project

---
+++ common/config/params.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'adminEmail' => 'admin@example.com',
+    'supportEmail' => 'support@example.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+    'frontendHostInfo' => 'http://example.com',
+    'backendHostInfo' => 'http://backend.example.com',
+    'user.passwordResetTokenExpire' => 3600,
+    'user.passwordMinLength' => 1,
+];
