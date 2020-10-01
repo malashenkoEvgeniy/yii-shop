6ae3a07d257d178c2a6a69fa6511ad3b8577e4b7
Evgeniy Malashenko <111k1808@gmail.com>
2020-10-01 09:32:04 +0300

    Added global backend access control

--- backend/config/main.php
+++ backend/config/main.php
@@ -38,10 +38,20 @@ return [
             'errorAction' => 'site/error',
         ],
         'backendUrlManager' => require __DIR__ . '/urlManager.php',
-            'frontendUrlManager' => require __DIR__ . '/../../frontend/config/urlManager.php',
-            'urlManager' => function () {
-                    return Yii::$app->get('backendUrlManager');
-                    },
+        'frontendUrlManager' => require __DIR__ . '/../../frontend/config/urlManager.php',
+        'urlManager' => function () {
+                return Yii::$app->get('backendUrlManager');
+                },
+        'as access' => [
+            'class' => 'yii\filters\AccessControl',
+            'except' => ['site/login', 'site/error'],
+            'rules' => [
+                [
+                    'allow' => true,
+                    'roles' => ['@'],
+                ],
+            ],
         ],
+    ],
 
 ];
